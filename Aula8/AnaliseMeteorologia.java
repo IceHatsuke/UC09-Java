90e53b3c3068762f624a27f16083364f2012a41d
Gabi-Rodrigues <142447684+IceHatsuke@users.noreply.github.com>
2026-02-27 21:33:10 -0300

    Add files via upload

--- Aula8/AnaliseMeteorologia.java
+++ Aula8/AnaliseMeteorologia.java
@@ -1,9 +1,9 @@
 package Aula8;
 
 public class AnaliseMeteorologia {
-
+    //CIDADES
     static String [] cidades = {"São Paulo", "Rio de Janeiro", "Belo Horizonte", "Curitiba", "Salvador"};
-
+    //TEMPERATURAS
     static double [][] temperatura = 
     {
         {20.5, 25.1},
@@ -12,7 +12,7 @@ public class AnaliseMeteorologia {
         {30.2, 20.5},
         {20.7, 11.3}
     };
-
+    //UMIDADES
     static int [][] umidade = 
     {
         {85, 60, 75},
@@ -21,7 +21,7 @@ public class AnaliseMeteorologia {
         {72, 58, 70},
         {80, 50, 68}
     };
-
+    //CALCULA A MEDIA DA TEMPERATURA
     public static double calcularMediaPonderadaTemperatura(double max, double min){
         if(max < -50 || max > 60 || min < -50 || min > 60)
         {
@@ -29,7 +29,7 @@ public class AnaliseMeteorologia {
         }
         return (max * 0.7) + (min * 0.3);
     }
-
+    //MOSTRA A CLASSIFICAÇÃO DO CLIMA
     public static void classificarClima(double tempMedia, int umidadeMedia){
         if(tempMedia >= 30 && umidadeMedia >= 75)
         {
@@ -45,7 +45,7 @@ public class AnaliseMeteorologia {
         }
 
     }
-
+    //MOSTRA QUAL CIDADE TEM A MAIOR DIFERENÇA ENTRE: TEMPERATURA MAXIMA E TEMPERATURA MINIMA
     public static int identificarCidadeComMaiorAmplitudeTermica(){
         int indice = 0;
         double maiorAmplitude = 0;
@@ -60,12 +60,12 @@ public class AnaliseMeteorologia {
         }
         return indice;
     }
-
+    //CALCULA O INDICE DE CALOR 
     public static double calcularIndiceCalor(double temp, int umidade){
         double indice = temp + 0.5 * (umidade / 100.0) * (temp - 20);
         return Math.round(indice * 10.0) / 10.0;
     }
-
+    //MOSTRA O NIVEL DE ALERTA
     public static int gerarAlertas(int cidadeIndex){
         double max = temperatura[cidadeIndex][0];
         double min = temperatura[cidadeIndex][1];
@@ -89,7 +89,7 @@ public class AnaliseMeteorologia {
     public static void calcularEstatisticasAvancadas(){
 
     }
-
+    //COMPARA QUAL CIDADE É MAIS QUENTE, A MEDIA PONDERADA E A MEDIA DA UMIDADE
     public static String compararCidades(int cidade1, int cidade2){
         double media1 = calcularMediaPonderadaTemperatura(temperatura[cidade1][0], temperatura[cidade1][1]);
         double media2 = calcularMediaPonderadaTemperatura(temperatura[cidade2][0], temperatura[cidade2][1]);
@@ -107,22 +107,22 @@ public class AnaliseMeteorologia {
         }
         else
         {
-            return "Temperaturas Iguais";
+            return "É Mais confortavel";
         }
     }
-
+    //GERA O RELATORIO DE TODAS AS CIDADES E CALCULA 
     public static void gerarRelatorioDetalhado(){
         System.out.printf("============================================================\n");
         System.out.printf("\tSISTEMA DE ANÁLISE METEOROLÓGICA INTELIGENTE\t\n");
         System.out.printf("============================================================\n");
         System.out.printf("\n\t\tANÁLISE DETALHADA POR CIDADE: \n");
         System.out.printf("\t---------------------------------------------\n");
-        System.out.printf("CIDADE | T.MAX | T.MED | UMID% | CLASSIFICAÇÃO | ALERTA\n");
+        System.out.printf("CIDADE | T.MAX | T.MED | UMID% | CLASSIFICAÇÃO     | ALERTA\n");
         System.out.printf("\t---------------------------------------------\n");
 
 
     }
-
+    //RETORNA MEDIA DA UMIDADE
     public static int calcularMediaUmidade(int cidadeIndex){
         int soma = 0;
         for (int i = 0; i < 3; i++) {
